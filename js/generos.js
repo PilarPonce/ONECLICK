d9770efabef6d5110c3dbc40590b504aff75f053
PilarPonce <69641834+PilarPonce@users.noreply.github.com>
2020-11-02 11:33:02 -0300

    java

--- js/generos.js
+++ js/generos.js
@@ -1,1 +1,11 @@
+window.addEventListener('load' ,function(){
+
 //https://api.themoviedb.org/3/movie/550?api_key=e8659a3dae8d207d31ba4797c06188c8
+
+
+
+
+
+
+
+})
--- js/seriesDetail.js
+++ js/seriesDetail.js
@@ -1,1 +1,8 @@
-//https://api.themoviedb.org/3/movie/550?api_key=e8659a3dae8d207d31ba4797c06188c8
+
+window.addEventListener('load' ,function(){
+
+
+
+
+
+})
