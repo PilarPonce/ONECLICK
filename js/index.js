82cf9b5d7ae343cdd01b54aed54f76fb788ca441
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-02 12:04:26 -0300

    index

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 window.onload = function (){
+
     fetch("//https://api.themoviedb.org/3/movie/550?api_key=e8659a3dae8d207d31ba4797c06188c8l") 
         .then(function(response){
             return response.json();
         })
+
         .then(function(data){
             console.log(data);
             
         })
+
         .catch(function(mensajeError){
             console.log(`hubo un error: ${mensajeError}`);
         });
+        
 };
