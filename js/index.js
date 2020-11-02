ce59fb7b9784a801ec5a2532adb776229177b1cf
PilarPonce <69641834+PilarPonce@users.noreply.github.com>
2020-11-02 11:52:28 -0300

    java con api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 window.addEventListener('load' ,function(){
 
+    fetch(`//https://api.themoviedb.org/3/movie/550?api_key=e8659a3dae8d207d31ba4797c06188c8`)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        console.log(data);
+
+
+    })
+    .catch(function (error) {
+         console.log('El error fue: ' + error);
+     })
+    
 
 
 
