05140e653a9bf8f4e0ab084ae50d4ed7e2f54c6b
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-04 19:03:29 -0300

    api key ok en todos

--- js/generos.js
+++ js/generos.js
@@ -1,5 +1,7 @@
 window.addEventListener('load' ,function(){
 
+    var key = `e8659a3dae8d207d31ba4797c06188c8`
+
     fetch(`//https://api.themoviedb.org/3/movie/550?api_key=e8659a3dae8d207d31ba4797c06188c8`)
     .then(function (response) {
         return response.json()
