354a07e22fa60486913985dbef89d2ccbe15170f
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-04 19:16:15 -0300

    link peliculas

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ window.onload = function (){
 
     var key = `e8659a3dae8d207d31ba4797c06188c8`
 
+    //https://developers.themoviedb.org/3/movies/get-top-rated-movies
+
     fetch(``)
     .then(function (response) {
         return response.json()
