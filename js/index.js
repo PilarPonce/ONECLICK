6c21b281df345e30d2cea29aab4cd3163241308c
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-10 16:43:01 -0300

    banner imagenes

--- js/index.js
+++ js/index.js
@@ -4,6 +4,30 @@ window.onload = function (){
 
     var linkimagen = "https://image.tmdb.org/t/p/w500"
 
+//--------------------------BANNER------------------------------------------------------
+fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US&page=1`)
+.then(function (response) {
+    return response.json()
+})
+.then(function (information) {
+    console.log(information);
+
+    var ul = document.querySelector (".bannerimg")
+
+
+    for (let i = 0; i < information.results.length; i++) {
+        var element = information.results[i];
+        ul.innerHTML  += `
+        <li>
+        <img src="${linkimagen}${element.poster_path}" alt="">
+        </li>` 
+    }
+})
+.catch(function (error) {
+     console.log('El error fue: ' + error);
+ })
+
+
 //--------------------------------------------------------------------------------
     fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
     .then(function (response) {
@@ -26,6 +50,8 @@ window.onload = function (){
     .catch(function (error) {
          console.log('El error fue: ' + error);
      })
+
+
 //--------------------------------------------------------------------------------
     fetch(`https://api.themoviedb.org/3/tv/airing_today?api_key=${apiKey}`)
     .then(function (response) {
