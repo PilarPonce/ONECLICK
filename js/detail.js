c6cb6addb81be4ff807b28b74aa277ae1e9b990a
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-19 20:13:47 -0300

    .

--- js/detail.js
+++ js/detail.js
@@ -3,9 +3,11 @@ window.onload = function (){
     var apiKey = `e8659a3dae8d207d31ba4797c06188c8`
     var linkimagen = "https://image.tmdb.org/t/p/w500"
 
-    
-    var id = 150; //no sabemos cual es el id
-    var tipo = serie;
+    var queryString = location.search;
+    var queryStringObj = new UrlSearchParams (queryString);
+
+    var id = queryStringObj.get ("id");
+    var tipo = queryStringObj.get ("tipo");
 
     if (tipo == "serie") {
         contenidoSerie (id);
@@ -21,7 +23,7 @@ window.onload = function (){
         serie.style.display = "none";
 
 
-        fetch(`https://api.themoviedb.org/3/movie` + id + apiKey)
+        fetch(`https://api.themoviedb.org/3/tv/{tv_id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
         .then(function (response) {
             return response.json()
         })
@@ -45,6 +47,20 @@ window.onload = function (){
         pelicula.style.display = "none";
         serie.style.display = "block";
 
+
+
+        fetch(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+
+
+        })
+        .catch(function (error) {
+            console.log('El error fue: ' + error);
+        })
 
     }
 
