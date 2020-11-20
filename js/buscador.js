7dca2c94189f8485ce0dc0d0fcbe1c4a3e35451e
PilarPonce <69641834+PilarPonce@users.noreply.github.com>
2020-11-20 11:26:06 -0300

    buscador html

--- js/buscador.js
+++ js/buscador.js
@@ -1,13 +1,36 @@
 window.addEventListener('load' ,function(){
 
-    var Apikey = `e8659a3dae8d207d31ba4797c06188c8`
+    var apikey = `e8659a3dae8d207d31ba4797c06188c8`
 
-    fetch(``)
+    var queryString = location.search
+
+    queryString = new URLSearchParams(queryString)
+  
+    var resultados = queryString.get("buscador")
+
+    var busqueda = document.querySelector (".busqueda")
+  
+
+
+    fetch(`https://api.themoviedb.org/3/search/company?api_key=e8659a3dae8d207d31ba4797c06188c8&page=1&${resultados}`   )
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data);
+        for (let index = 0; index < data.length; index++) {
+            const element = data[index];
+            busqueda.innerHTML += `
+            
+            <a href="index.html?id= ${element.id}"> 
+            <h2>${element.name}</h2>
+            </a>
+
+            <img src="${element.logo_path}" alt="">
+
+            `
+            
+        }
 
 
     })
