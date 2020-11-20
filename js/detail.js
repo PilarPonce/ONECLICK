a87a0258d37c9786117b229726d8a87ccb3b6ee9
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-20 11:29:43 -0300

    detail

--- js/detail.js
+++ js/detail.js
@@ -4,16 +4,18 @@ window.onload = function (){
     var linkimagen = "https://image.tmdb.org/t/p/w500"
 
     var queryString = location.search;
-    var queryStringObj = new UrlSearchParams (queryString);
+    var queryStringObj = new URLSearchParams(queryString);
 
     var id = queryStringObj.get ("id");
     var tipo = queryStringObj.get ("tipo");
+    console.log(tipo);
 
     if (tipo == "serie") {
         contenidoSerie (id);
         
     } else if (tipo == "pelicula") {
-        contenidoPelicula (id)
+        contenidoPelicula (id);
+        
     } else {
         contenidoGeneros (id)
     }
@@ -21,28 +23,69 @@ window.onload = function (){
     function contenidoSerie (id){
 
         var serie = document.querySelector (".serie");
-
+    
         serie.style.display = "none";
         generos.style.display = "none";
-
-
-        fetch(`https://api.themoviedb.org/3/tv/{tv_id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
+    
+    
+        fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
         .then(function (response) {
             return response.json()
         })
         .then(function (data) {
             console.log(data);
+    
+            var pelicula = document.querySelector (".pelicula")
 
-
+            for (let i = 0; i < information.results.length; i++) {
+                var element = information.results[i];
+                pelicula.innerHTML += `
+            
+                <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
+                    <div class="uk-card-media-left uk-cover-container">
+                        <img class="imagen" src="${linkimagen}${element.poster_path}" alt="" uk-cover>
+                        <canvas width="600" height="400"></canvas>
+                    </div>
+                    <div>
+                        <div class="uk-card-body">
+                            <h3 class=" titulo uk-card-title">Media Left</h3>
+                            <p class="descripcion">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                            <p class="promedioVotos"> </p>
+                        </div>
+                    </div>
+                </div>
+        
+                <div class="uk-card uk-card-default uk-width-1-2@m ">
+                    <div class="uk-card-header">
+                        <div class="uk-grid-small uk-flex-middle" uk-grid>
+                            <div class="uk-width-auto">
+                                <img class="uk-border-circle" width="40" height="40" src="IMAGENES/footer/fotoavatar.jpg">
+                            </div>
+                            <div class="uk-width-expand">
+                                <h3 class=" resenas uk-card-title uk-margin-remove-bottom">Reseñas</h3>
+                                <p class=" uk-text-meta uk-margin-remove-top"><time datetime="2016-04-01T19:00">April 01, 2016</time></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="uk-card-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    </div>
+                    <div class="uk-card-footer">
+                        <a href="#" class="uk-button uk-button-text">Read more</a>
+                    </div>
+                </div>
+               `
+            }   
+    
+    
         })
         .catch(function (error) {
             console.log('El error fue: ' + error);
         })
-
-
+    
     }
 
-
+    
     function contenidoPelicula (id){
 
         var pelicula = document.querySelector (".pelicula");
@@ -53,7 +96,7 @@ window.onload = function (){
 
 
 
-        fetch(`https://api.themoviedb.org/3/movie/{movie_id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
         .then(function (response) {
             return response.json()
         })
@@ -78,5 +121,7 @@ window.onload = function (){
 
         
     }
+    
 
 }
+
