f78c1ec9d8b6282a4cef9f5983211c5a491c4c08
PilarPonce <69641834+PilarPonce@users.noreply.github.com>
2020-11-20 13:43:15 -0300

    buscador

--- js/buscador.js
+++ js/buscador.js
@@ -1,35 +1,43 @@
 window.addEventListener('load' ,function(){
 
     var apikey = `e8659a3dae8d207d31ba4797c06188c8`
+    var linkimagen = "https://image.tmdb.org/t/p/w500"
 
-    var queryString = location.search
+    var queryString = location.search;
 
-    queryString = new URLSearchParams(queryString)
+    queryString = new URLSearchParams(queryString);
   
-    var resultados = queryString.get("buscador")
+    var buscador = document.querySelector (".buscador");
 
-    var busqueda = document.querySelector (".busqueda")
-  
+    var resultados = queryString.get(".resultados");
 
 
-    fetch(`https://api.themoviedb.org/3/search/company?api_key=e8659a3dae8d207d31ba4797c06188c8&page=1&${resultados}`   )
+    fetch(`https://api.themoviedb.org/3/search/multi?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US&query=${resultados}&page=1&include_adult=false`)
     .then(function (response) {
         return response.json()
     })
     .then(function (data) {
         console.log(data);
-        for (let index = 0; index < data.length; index++) {
-            const element = data[index];
-            busqueda.innerHTML += `
-            
-            <a href="index.html?id= ${element.id}"> 
-            <h2>${element.name}</h2>
-            </a>
+        var arrayDePeliculas = data.results;
 
-            <img src="${element.logo_path}" alt="">
 
-            `
+        for (let index = 0; index < data.results.length; index++) {
+            const element = data.results[index];
+            console.log (element.media_type)
+
+            if (element.media_type == "tv" || element.media_type == "movie") {
+                buscador.innerHTML += `
             
+                <a href="index.html?id= ${element.id}"> 
+                <h2>${element.original_title}</h2>
+                </a>
+    
+                <img src="${linkimagen}${element.poster_path}" alt="">
+                ` ;
+                
+            }  
+            
+    
         }
 
 
