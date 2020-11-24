5e6e5c6e9128a4040f74962e8d8f75b747c23aff
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-24 14:50:28 -0300

    sin array

--- js/generos.js
+++ js/generos.js
@@ -52,8 +52,6 @@ window.onload = function () {
         }
     })
 
-    Array = []
-
     //---------------ACCION---------------------------------------------------------
     
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apikey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28`)
