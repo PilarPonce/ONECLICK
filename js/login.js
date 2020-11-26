4a6462230161e8b6c54e782f81bc118742cf7295
PilarPonce <69641834+PilarPonce@users.noreply.github.com>
2020-11-26 18:23:39 -0300

    push

--- js/login.js
+++ js/login.js
@@ -1,45 +1,68 @@
 window.addEventListener (`load`,function(){
 
-var nombre = document.getElementById("name")
-var email = document.getElementById("email")
-var pass = document.getElementById("password")
-var form = document.getElementById("form")
-var avisando = document.getElementById("aviso")
+    /*
+window.addEventListener("load",function(){ 
 
-form.addEventListener("submit", enviar=>{
-    enviar.preventDefault()
-    var aviso = ""
-    var entrar = false
-    var elEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{3})+$/
-    avisando.innerHTML = ""
-    if(nombre.value.length <1){
-        aviso += `El nombre es invalido <br>`
-        entrar = true
-    }
-    if(!elEmail.test(email.value)){
-        aviso += `El email es invalido <br>`
-        entrar = true
-    }
-    if(pass.value.length < 6){
-        aviso += `La contraseña es invalida <br>`
-        entrar = true
-    }
+    var contenidoBusqueda = document.querySelector(`[name=nombreusuario]`)
+    var form = document.querySelector(`#form`)
 
-    if(entrar){
-        avisando.innerHTML = aviso
-    }else{
-        avisando.innerHTML = "Enviado"
-    }
+
+    form.addEventListener(`submit`, function (event) {
+       
+        if (contenidoBusqueda.value == ``) {
+        event.preventDefault()
+        contenidoBusqueda.classList.add(`error`)
+        form.innerHTML = `<div>
+        <input name="nombreusuario" type="text" placeholder="Nombre...">
+        <button id="botonsubmit" type="submit">Enviar</button> </div>
+        <p id="errorp">Error: Porfavor recargue la pestaña e ingrese su nombre.</p>
+        `
+        }
+            })
+*/
 })
 
-})
 
-var queryString = location.search;
+    var nombre = document.getElementById("name")
+    var email = document.getElementById("email")
+    var pass = document.getElementById("password")
+    var form = document.getElementById("form")
+    var avisando = document.getElementById("aviso")
+    
+    form.addEventListener("submit", enviar=>{
+        enviar.preventDefault()
+        var aviso = ""
+        var entrar = false
+        var elEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{3})+$/
+        avisando.innerHTML = ""
+        if(nombre.value.length <1){
+            aviso += `El nombre es invalido <br>`
+            entrar = true
+        }
+        if(!elEmail.test(email.value)){
+            aviso += `El email es invalido <br>`
+            entrar = true
+        }
+        if(pass.value.length < 6){
+            aviso += `La contraseña es invalida <br>`
+            entrar = true
+        }
+    
+        if(entrar){
+            avisando.innerHTML = aviso
+        }else{
+            avisando.innerHTML = "Enviado"
+        }
+    })
+    
+    var queryString = location.search;
+    
+    queryString = new URLSearchParams(queryString);
+    
+    var login = document.querySelector(".completa");
+    
+    var loQueCompletoElUsuario = queryString.get("input");
 
-queryString = new URLSearchParams(queryString);
 
-var login = document.querySelector(".completa");
-
-var loQueCompletoElUsuario = queryString.get("input");
 
 
