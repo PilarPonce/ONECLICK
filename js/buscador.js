a7671a21b7c8c98c5b94ae9ac7f1dd573859e01f
nicole-lapidus <nlapidus@udesa.edu.ar>
2020-11-26 12:00:30 -0300

    buscador ok

--- js/buscador.js
+++ js/buscador.js
@@ -1,10 +1,9 @@
-window.addEventListener('load' ,function(){
-    var load =  document.querySelector (`.spinner`)
+window.addEventListener('load', function () {
+    var load = document.querySelector(`.spinner`)
     load.style.display = "none";
-    
-    var loQueQueremos = document.querySelector (`.noqueremos`)
-    setTimeout(function(){  }, 9000);
-    
+
+    var loQueQueremos = document.querySelector(`.noqueremos`)
+    setTimeout(function () { }, 9000);
 
 
     var apikey = `e8659a3dae8d207d31ba4797c06188c8`
@@ -13,39 +12,102 @@ window.addEventListener('load' ,function(){
     var queryString = location.search;
 
     queryString = new URLSearchParams(queryString);
-  
-    var buscador = document.querySelector (".buscador");
+
+    var buscador = document.querySelector(".buscador");
 
     var loQueBuscoElUsuario = queryString.get("inputbuscador");
 
+
+
+
+    var todoBoton = document.querySelector(".todoboton")
+
+    todoBoton.addEventListener("change", function () { //change es cuando cambia de seleccion, de option. 
+        var ul = document.querySelector(".listapelis")
+        ul.innerHTML = "";
+        //se fija cual es el valor
+        if (todoBoton.value === "Peliculas") { //si el valor es pelicula que muestre esto:
+            fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apikey}&language=en-US&query=${loQueBuscoElUsuario}&page=1&include_adult=false`)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var arrayDePeliculas = data.results; //todas las peliculas que encuentra
+
+
+                    for (let index = 0; index < data.results.length; index++) {
+                        const element = data.results[index];
+                        console.log(element.media_type)
+
+
+                        if (element.media_type == "movie") { //aca se fija si son peliculas y si son las muestra
+                            ul.innerHTML += `
+
+                <li> 
+                <a href="detail.html?tipo=pelicula&id=${element.id}"> 
+                <img src="${linkimagen}${element.poster_path}" alt="">
+                </a>
+                </li>`
+                        }
+                    }
+
+
+                })
+                .catch(function (error) {
+                    console.log('El error fue: ' + error);
+                })
+
+        } else if (todoBoton.value == "Series") { //si el tipo es serie
+            fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apikey}&language=en-US&query=${loQueBuscoElUsuario}&page=1&include_adult=false`)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var arrayDePeliculas = data.results;
+
+                    var ul = document.querySelector(".listapelis")
+
+                    for (let index = 0; index < data.results.length; index++) {
+                        const element = data.results[index];
+                        console.log(element.media_type)
+
+                        if (element.media_type == "tv") {
+                            ul.innerHTML += `
     
-    
-    
-    var opcion1 = document.querySelector (".opcion1")
-    
-    opcion1.addEventListener ("click", function (){
+                    <li> 
+                    <a href="detail.html?tipo=serie&id=${element.id}"> 
+                    <img src="${linkimagen}${element.poster_path}" alt="">
+                    </a>
+                    </li>
+                    
+                    `
+                        }
+                    }
 
-       alert ("apretasre boton");
 
-    });
+                })
+                .catch(function (error) {
+                    console.log('El error fue: ' + error);
+                })
+        } else { //si no tiene ningun filtro (seleccion)
+            fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apikey}&language=en-US&query=${loQueBuscoElUsuario}&page=1&include_adult=false`)
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data);
+                    var arrayDePeliculas = data.results;
 
+                    var ul = document.querySelector(".listapelis")
 
-    fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apikey}&language=en-US&query=${loQueBuscoElUsuario}&page=1&include_adult=false`)
-    .then(function (response) {
-        return response.json()
-    })
-    .then(function (data) {
-        console.log(data);
-        var arrayDePeliculas = data.results;
+                    for (let index = 0; index < data.results.length; index++) {
+                        const element = data.results[index];
+                        console.log(element.media_type)
 
-        var ul = document.querySelector (".listapelis")
-
-        for (let index = 0; index < data.results.length; index++) {
-            const element = data.results[index];
-            console.log (element.media_type)
-
-            if (element.media_type == "tv") {
-                ul.innerHTML += `
+                        if (element.media_type == "tv") {
+                            ul.innerHTML += `
 
                 <li> 
                 <a href="detail.html?tipo=serie&id=${element.id}"> 
@@ -54,23 +116,65 @@ window.addEventListener('load' ,function(){
                 </li>
                 
                 `
-            }  if (element.media_type == "movie") {
-                ul.innerHTML += `
+                        } if (element.media_type == "movie") {
+                            ul.innerHTML += `
 
                 <li> 
                 <a href="detail.html?tipo=pelicula&id=${element.id}"> 
                 <img src="${linkimagen}${element.poster_path}" alt="">
                 </a>
                 </li>`
-            } 
+                        }
+                    }
+
+
+                })
+                .catch(function (error) {
+                    console.log('El error fue: ' + error);
+                })
         }
+    });
+    //lo dejamos por cuando arranca. Si no usamos el filtrador, necesitamos tener esto
+    fetch(`https://api.themoviedb.org/3/search/multi?api_key=${apikey}&language=en-US&query=${loQueBuscoElUsuario}&page=1&include_adult=false`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            var arrayDePeliculas = data.results;
+
+            var ul = document.querySelector(".listapelis")
+
+            for (let index = 0; index < data.results.length; index++) {
+                const element = data.results[index];
+                console.log(element.media_type)
+
+                if (element.media_type == "tv") {
+                    ul.innerHTML += `
+
+                <li> 
+                <a href="detail.html?tipo=serie&id=${element.id}"> 
+                <img src="${linkimagen}${element.poster_path}" alt="">
+                </a>
+                </li>
+                
+                `
+                } if (element.media_type == "movie") {
+                    ul.innerHTML += `
+
+                <li> 
+                <a href="detail.html?tipo=pelicula&id=${element.id}"> 
+                <img src="${linkimagen}${element.poster_path}" alt="">
+                </a>
+                </li>`
+                }
+            }
 
 
-    })
-    .catch(function (error) {
-         console.log('El error fue: ' + error);
-     })
-    
+        })
+        .catch(function (error) {
+            console.log('El error fue: ' + error);
+        })
 
 
 
--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,4 @@
-window.onload = function (){ 
+window.onload = function () {
 
     var apiKey = `e8659a3dae8d207d31ba4797c06188c8`
     var linkimagen = "https://image.tmdb.org/t/p/w500"
@@ -6,33 +6,33 @@ window.onload = function (){
     var queryString = location.search;
     var queryStringObj = new URLSearchParams(queryString);  //
 
-    var id = queryStringObj.get ("id");
-    var tipo = queryStringObj.get ("tipo");
+    var id = queryStringObj.get("id");
+    var tipo = queryStringObj.get("tipo");
 
-    
+
     if (tipo == "serie") {
-        contenidoTv (id);
-        
+        contenidoTv(id);
+
     } else if (tipo == "pelicula") {
-        contenidoPelicula (id);
-        
+        contenidoPelicula(id);
+
     } else {
-        contenidoGeneros (id)
+        contenidoGeneros(id)
     }
 
-    function contenidoPelicula (id){
+    function contenidoPelicula(id) {
         document.querySelector('.tv').style.display = "none";
         document.querySelector('.generos').style.display = "none";
-        
-    
+
+
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}&language=en-US&page=1`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
-    
-            var pelicula = document.querySelector (".pelicula")
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
+
+                var pelicula = document.querySelector(".pelicula")
                 pelicula.innerHTML += `
             
                 <div class=" cajaprincipal uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
@@ -41,7 +41,8 @@ window.onload = function (){
                     </div>
                     <div>
                         <div class=" cajainfo uk-card-body">
-                            <h3 class=" titulo uk-card-title">${data.title}</h3>  <a href="" class ="estrella" uk-icon ="star"></a>
+                            <h3 class=" titulo uk-card-title">${data.title}</h3> <a href="#" role="button" class="estrella" uk-icon="star"></a>
+                            <a href="#" role="button" class="eliminar" uk-icon="trash"></a>
                             <h5 class="promedioVotos"> Promedio de votos: ${data.vote_average} </h5>
                             <p class="descripcion">${data.overview}</p>
                             
@@ -50,28 +51,29 @@ window.onload = function (){
                 </div>
     
                `
+               //el role = button y el #: esto es un boton no un link, no me lleves a otro lado
             })
             .catch(function (error) {
-            console.log('El error fue: ' + error);
-        })
+                console.log('El error fue: ' + error);
+            })
 
         //REVIEWS MOVIE (en peliculas hay seccion peliculas reviews)
         fetch(`https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${apiKey}&language=en-US&page=1`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
 
-            var pelicula = document.querySelector (".pelicula")
-            var resena = "";
-            var autor = "";
-            for (let i = 0; i < data.results.length; i++) {
-                const element = data.results[i];
-                resena += `<p>${element.content}</p>`
-                autor += `${element.author}`
-            }
-            
+                var pelicula = document.querySelector(".pelicula")
+                var resena = "";
+                var autor = "";
+                for (let i = 0; i < data.results.length; i++) {
+                    const element = data.results[i];
+                    resena += `<p>${element.content}</p>`
+                    autor += `${element.author}`
+                }
+
                 pelicula.innerHTML += `
         
                 <div class=" resenastotal uk-card uk-card-default uk-width-1-2@m ">
@@ -94,36 +96,60 @@ window.onload = function (){
                     </div>
                 </div>
                `
+               configurarFavoritos (tipo,id);
             })
             .catch(function (error) {
-            console.log('El error fue: ' + error);
-        })
-    
+                console.log('El error fue: ' + error);
+            })
+
+        //TRAILER PELI
+        //https://stackoverflow.com/questions/21845089/get-youtube-video-id-from-embed-iframe-code
+        //Buscamos embedyoutubeidvideo (como meter videos de youtube en mi pagina)
+        fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
+                var videoHolder = document.querySelector (`.videoholder`)
+                var youtubeKey = data.results[0].key
+                if (youtubeKey) {
+                    videoHolder.innerHTML += `
+                    <h2> Videos relacionados: </h2>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeKey}?rel=0" frameborder="0" allowfullscreen></iframe>
+                    `
+                }
+                
+            })
+            .catch(function (error) {
+                console.log('El error fue: ' + error);
+            })
+
     }
 
-    
-    function contenidoTv (id){
+
+    function contenidoTv(id) {
 
         document.querySelector('.pelicula').style.display = "none";
         document.querySelector('.generos').style.display = "none";
 
 
         fetch(`https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}&language=en-US&page=1`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
 
-            var tv = document.querySelector (".tv")
-            var seriegenero = "";
-            for (let i = 0; i < data.genres.length; i++) {
-                const element = data.genres[i];
-                seriegenero +=`<p class="generoDetalle"> ${element.name} </p>`
-            }
-            
-            
-            tv.innerHTML += `
+                var tv = document.querySelector(".tv")
+                var seriegenero = "";
+                for (let i = 0; i < data.genres.length; i++) {
+                    const element = data.genres[i];
+                    seriegenero += `<p class="generoDetalle"> ${element.name} </p>`
+                }
+
+
+                tv.innerHTML += `
             
             <div class= "cajaprincipal">
                 <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2@s uk-margin" uk-grid>
@@ -132,7 +158,8 @@ window.onload = function (){
                 </div>
                 <div>
                     <div class=" cajainfo uk-card-body">
-                        <h3 class=" titulo uk-card-title">${data.name}</h3>  
+                        <h3 class=" titulo uk-card-title">${data.name}</h3>  <a href="#" role="button" class="estrella" uk-icon="star"></a>
+                        <a href="#" role="button" class="eliminar" uk-icon="trash"></a>
                         <a href="detail.html?tipo=generos&id=${id}"> ${seriegenero}</a>
                         <h4 class="promedioVotos">Fecha de salida: ${data.last_air_date}</h4>
                         <p class="descripcion">${data.overview}</p>
@@ -141,18 +168,44 @@ window.onload = function (){
             </div>
             `
 
+            configurarFavoritos (tipo,id);
 
+            })
+            .catch(function (error) {
+                console.log('El error fue: ' + error);
+            })
+
+        //TRAILER PELI
+        //https://stackoverflow.com/questions/21845089/get-youtube-video-id-from-embed-iframe-code
+        //Buscamos embedyoutubeidvideo (como meter videos de youtube en mi pagina)
+        fetch(`https://api.themoviedb.org/3/tv/${id}/videos?api_key=e8659a3dae8d207d31ba4797c06188c8&language=en-US`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
+            var videoHolder = document.querySelector (`.videoholder`)
+            var youtubeKey = data.results[0].key
+            if (youtubeKey) {
+                videoHolder.innerHTML += `
+                <h2> Videos relacionados: </h2>
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/${youtubeKey}?rel=0" frameborder="0" allowfullscreen></iframe>
+                `
+            }
+            
         })
         .catch(function (error) {
             console.log('El error fue: ' + error);
         })
     }
-    
-//generos detail
+
+
+
+    //generos detail
     function contenidoGeneros(id) {
         document.querySelector('.pelicula').style.display = "none";
         document.querySelector('.tv').style.display = "none";
-       
+
         var tituloGenero = document.querySelector('.titulogenero')
         var tituloVista = "";
 
@@ -182,36 +235,43 @@ window.onload = function (){
         tituloGenero.innerHTML = `${tituloVista}`
 
         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${id}`)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            console.log(data);
+            .then(function (response) {
+                return response.json()
+            })
+            .then(function (data) {
+                console.log(data);
 
-            var generos = document.querySelector(".listageneros");
+                var generos = document.querySelector(".listageneros");
 
-            for (let i = 0; i < data.results.length; i++) {
-                const element = data.results[i];
-                
-                generos.innerHTML += `
+                for (let i = 0; i < data.results.length; i++) {
+                    const element = data.results[i];
+
+                    generos.innerHTML += `
                     <a href="detail.html?tipo=pelicula&id=${element.id}">
                     <li>
                         <img class = "fotodet" src="${linkimagen}${element.poster_path}" alt="">
                     </li>
                     <a/>
-                    `  
-            }
-        
+                    `
+                }
+
             })
             .catch(function (error) {
-            console.log('El error fue: ' + error);
-        })
+                console.log('El error fue: ' + error);
+            })
     }
+
+
     
 
- //favs
+
+}
+
+const configurarFavoritos = function (tipo,id) {
+    //favs
     var favs = localStorage.getItem("favoritos") //trae string correspondiente al lugar favoritos
     var favoritos = document.querySelector(".estrella")
+    var sacar = document.querySelector(".eliminar")
     var arrayDeFavoritos = "";
 
     if (favs === null) {
@@ -220,18 +280,48 @@ window.onload = function (){
         arrayDeFavoritos = JSON.parse(favs) //lo hace objeto para poder manipularlo
     }
 
-    favoritos.addEventListener ("click", function (){
-    alert("apretaste el boton!")
+    for (let i = 0; i < arrayDeFavoritos.length; i++) { //este for hace que por cada favorito en favoritos se fija si esta y dependiendo de si esta o no, muestra un boton o el otro
+        const element = arrayDeFavoritos[i];
+        if (element.id == id) {
+            sacar.style.display = "block";
+            favoritos.style.display = "none";
+            break; //me saca del for incluso si no termine la condicion. Lo hago porque si ya encontre el favorito, no tengo que seguir buscandolo
+        } else {
+            sacar.style.display = "none";
+            favoritos.style.display = "block";
+        }
+    } 
 
-     
-        arrayDeFavoritos.push ({
-            tipo: tipo,
-            id: id
+    if (arrayDeFavoritos.length === 0) { //si un array esta vacia, la longitud del array es 0, osea estoy seguro que no esta en favoritos, muestro el de agregar (estrellita)
+        sacar.style.display = "none";
+        favoritos.style.display = "block";
+    }
+
+    favoritos.addEventListener("click", function () { //cuando agrego, muestra que lo agregue, cambia el boton
+        alert("Lo agregaste a favoritos!")
+
+        sacar.style.display = "block";
+        favoritos.style.display = "none";
+
+        arrayDeFavoritos.push({ //aca agrego un objeto a mis favoritos
+            tipo: tipo, //le estoy agregando un elemento a mi objeto con el identificador "tipo" con el valor de esa variable
+            id: id //le estoy agregando un elemento a mi objeto con el identificador "id" con el valor de esa variable
         });
 
-        localStorage.setItem("favoritos",JSON.stringify(arrayDeFavoritos))
+        localStorage.setItem("favoritos", JSON.stringify(arrayDeFavoritos)) //agarro nuevos favoritos y la guardo en el local storage. El stringify convierte el objeto de java script en un string
     })
-  
 
-   
+    sacar.addEventListener("click", function () { //cuando clickeo, 
+        alert ("Lo sacaste de favoritos!")
+        sacar.style.display = "none";
+        favoritos.style.display = "block";
+        for (let i = 0; i < arrayDeFavoritos.length; i++) { //me fijo en todos los favoritos cual es la posicion de mi fav que quiero borrar y cuando coinciden los id, los borro
+            const element = arrayDeFavoritos[i];
+            if (element.id == id) {
+                arrayDeFavoritos.splice (i,1); //aca digo que en i borre 1 elemento.
+                localStorage.setItem("favoritos", JSON.stringify(arrayDeFavoritos))
+            }
+        }
+    })
+
 }
